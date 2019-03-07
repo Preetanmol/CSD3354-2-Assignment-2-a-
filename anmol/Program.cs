ed602822eb2520afae9586d70310d17c901affe3
visualstudio <visualstudio@230-PC19>
2019-03-07 14:01:39 -0500

    commit

--- anmol/Program.cs
+++ anmol/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//Anmolpreet Singh
+//C0726606
+//Jaskaran Singh
+//C0731667
+//CSD3352 Section 2
+//Assignment 2
+//March 6, 2019
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +14,35 @@ using System.Threading.Tasks;
 
 namespace anmol
 {
+    public class DelegateExercises
+    {
+        public delegate void MyDelgate();
+
+
+        void Method1()
+        {
+
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelgate myDelgate = new MyDelgate(Method1);
+
+            myDelgate();
+        }
+
+
+    
+    
+        
+    }
+}
+
     class Program
     {
         static void Main(string[] args)
         {
         }
     }
-}
+
